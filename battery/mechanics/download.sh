439263ce762780ce714dc95859855156002b1a57
Fabian Bodensteiner <fabian.bodensteiner@wordlcoin.org>
2023-01-27 12:04:47 +0100

    updated engineering files

--- battery/mechanics/download.sh
+++ battery/mechanics/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://worldcoin-orb-hardware-battery.s3.amazonaws.com/84-00008-D.1-0.STEP
+wget https://worldcoin-orb-hardware-battery.s3.amazonaws.com/84-00008.stp
--- orb/mechanics/download.sh
+++ orb/mechanics/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://worldcoin-orb-hardware.s3.amazonaws.com/orb/mechanics/84-00005-B.1-0.step
+wget https://worldcoin-orb-hardware.s3.amazonaws.com/orb/mechanics/84-00005.stp
