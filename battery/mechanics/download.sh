fb2a6193f4fedf1a2262ac127ff5500f80bb25ab
tobiasweidner <tobias.weidner@worldcoin.org>
2024-03-22 13:50:52 +0100

    updated STEP files for battery and orb

--- battery/mechanics/download.sh
+++ battery/mechanics/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://worldcoin-orb-hardware-battery.s3.amazonaws.com/84-00008.stp
+wget https://worldcoin-orb-hardware-battery.s3.amazonaws.com/84-00008-H.1-0.STEP
--- orb/mechanics/download.sh
+++ orb/mechanics/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://worldcoin-orb-hardware.s3.amazonaws.com/orb/mechanics/84-00005.stp
+wget https://worldcoin-orb-hardware-battery.s3.amazonaws.com/84-00005-F.1-0.STEP
